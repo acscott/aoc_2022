bc5b5d055a5af097b3257533ab7705085934e676
ascott314 <adam.scott@noirlab.edu>
2022-12-09 18:27:40 -0800

    day6 completed

---
+++ day6/pt2.py
@@ -0,0 +1,9 @@
+with open("input.txt", "r") as f:
+    signal = f.read().splitlines()[0]
+
+pos = 0
+while True:
+    if len(set(signal[pos:(pos+14)])) == 14:
+        break
+    pos += 1
+print(f"pos={pos+14}")
